2aa30f39370681329430eb45bfd01389bad25224
A. Glez. B <12277525+AGlezB@users.noreply.github.com>
2023-08-20 07:39:02 -0400

    Add description field (to be manually edited in the .json)

--- Model/SaveData.cs
+++ Model/SaveData.cs
@@ -2,6 +2,7 @@
 {
 	public class SaveData
 	{
+		public string Description { get; set; } = string.Empty;
 		public IEnumerable<OreMarketPrice>? MarketPrices { get; set; }
 		public OreChunk? Chunk1 { get; set; }
 		public OreChunk? Chunk2 { get; set; }
